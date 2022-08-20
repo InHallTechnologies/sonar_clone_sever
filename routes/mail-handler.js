3ccacc95d9ac766365271e9a17c8d4a98b85fa04
Krati Shrivastava <shrivastava.krati.ks@gmail.com>
2022-08-21 03:26:12 +0530

    Accept-Reject

--- routes/mail-handler.js
+++ routes/mail-handler.js
@@ -18,7 +18,7 @@ mailRouter.get("/send-exceptional-request", (req, res) => {
         debug: true,
         auth: {
             user: "krati@inhall.in",
-            pass: "qazplm@098" 
+            pass: "Rishabhmylove#1" 
         }
     });
 
@@ -40,6 +40,48 @@ mailRouter.get("/send-exceptional-request", (req, res) => {
 
     mailTransport.sendMail(mailOptions).then((result) => {
         res.send(result)
+    })
+})
+
+mailRouter.get('/send-exceptional-request-mail-response', (req, res) => {
+    const { projectId, userId, time, reason, requestUID, mailTo, status } = req.query;
+    const mailTransport = nodemailer.createTransport({    
+        host: "smtpout.secureserver.net",  
+        secure: true,
+        secureConnection: false, // TLS requires secureConnection to be false
+        tls: {
+            ciphers:'SSLv3'
+        },
+        requireTLS:true,
+        port: 465,
+        debug: true,
+        auth: {
+            user: "krati@inhall.in",
+            pass: "Rishabhmylove#1" 
+        }
+    });
+
+    const mailOptions = {
+        from: `krati@inhall.in`,
+        to: mailTo,
+        subject: `Response: Exceptional Approval for ${projectId}`,
+        html: `
+            <h3>Exceptional Approval for <strong>${projectId}</strong></h3>
+            <p><strong>Request By: </strong> Rishabh Verma</p>
+            <p><strong>Reason: </strong> ${reason}</p>
+            <p><strong>requestByUID: </strong> ${userId}</p>
+            <p><strong>requestUID: </strong> ${requestUID}</p>
+            <p><strong>requestTime: </strong> ${time}</p>
+            <hr />
+            <p>Your request for exceptional approval is ${status}ED.</p>
+            <hr />
+            <h4>Access the project <a href="http://localhost:3000/?viewport=IZAnalyser">here</a></h4>
+        `,
+    };
+
+    mailTransport.sendMail(mailOptions).then((result) => {
+        res.send(result);
+        console.log(result);
     })
 })
 
